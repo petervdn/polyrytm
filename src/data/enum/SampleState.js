00b697a6e94914b3d8717f270c543cca2751506f
Peter van der Noord <peterdunord@gmail.com>
2019-02-09 21:33:45 +0100

    Add missing SampleState file

---
+++ src/data/enum/SampleState.js
@@ -0,0 +1,7 @@
+export default {
+  READY: 'ready', // todo better name for this one?
+  UPLOADING: 'uploading',
+  WAITING_FOR_DB_ENTRY: 'waiting-for-db-entry',
+  DELETING_FILE: 'deleting-file',
+  WAITING_FOR_DB_REMOVAL: 'waiting-for-db-removal',
+};
