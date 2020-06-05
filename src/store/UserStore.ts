2cc84a5219a9755378f4d8949c356763dedd237f
Peter van der Noord <peterdunord@gmail.com>
2020-06-06 01:23:54 +0200

    Set admin-value from customClaim in store

--- src/store/UserStore.ts
+++ src/store/UserStore.ts
@@ -5,20 +5,24 @@ import RootStore from './RootStore';
 
 export default class UserStore extends AbstractStore {
   @observable user: firebase.User | null = null;
+  @observable isAdmin = false;
 
   constructor(rootStore: RootStore) {
     super(rootStore);
 
     firebase.auth().onAuthStateChanged((user) => {
-      this.user = user;
+      if (user) {
+        user.getIdTokenResult().then((result) => {
+          this.isAdmin = result.claims.admin === true;
+          this.user = user;
+        });
+      } else {
+        this.user = null;
+      }
     });
   }
 
   @computed get userId() {
     return this.user ? this.user.uid : undefined;
-  }
-
-  @computed get isAdmin() {
-    return true;
   }
 }
